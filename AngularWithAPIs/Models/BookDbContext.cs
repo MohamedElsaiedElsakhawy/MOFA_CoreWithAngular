38aae950c377d6199199dd28489123387f0b0f68
ELSAKHAWY <ELSAKHAWY@DESKTOP-9FPHRGE>
2022-07-03 08:30:26 +0300

    init

---
+++ AngularWithAPIs/Models/BookDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AngularWithAPIs.Models
+{
+    public partial class BookDbContext : DbContext
+    {
+        public BookDbContext()
+        {
+        }
+
+        public BookDbContext(DbContextOptions<BookDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BookDb;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.ToTable("Book");
+
+                entity.Property(e => e.Author).HasMaxLength(50);
+
+                entity.Property(e => e.PublishedAt).HasColumnType("datetime");
+
+                entity.Property(e => e.Title).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ AngularWithAPIs/Program.cs
@@ -0,0 +1,42 @@
+using AngularWithAPIs.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BookDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookDb")); 
+});
+
+builder.Services.AddCors(op => op.AddPolicy("CORS", x =>
+{
+    x.AllowAnyHeader();
+    x.AllowAnyMethod();
+    x.AllowAnyOrigin();
+}));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors("CORS");
+
+app.Run();
